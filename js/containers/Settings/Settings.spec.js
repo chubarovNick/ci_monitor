66fc794cd619a4a4fd9d39158c188c7d4c0473d0
Nick Chubarov <nick.chubarov@gmail.com>
2016-01-17 21:10:08 +0400

    Revert mocha-jsdom package
    - add bugs section to package.json
    - add license section to package.json
    - add author section to package.json

--- js/containers/Settings/Settings.spec.js
+++ js/containers/Settings/Settings.spec.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import configureStore from '../../store/configureStore'
 import Settings from './Settings'
 
-import jsdom from 'jsdom'
+import jsdom from 'mocha-jsdom'
 
 import { Toggle } from 'material-ui'
 
